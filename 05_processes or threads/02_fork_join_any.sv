264e82f52868b3f3632527bb941ec3ce8bf9dbac
ANBUSELVAM V <anbuselvamv27@gmail.com>
2026-02-11 19:45:31 +0530

    Refactor comments and add display loop in fork_join_any
    Updated comments for clarity and added a counter display loop.

--- 05_processes or threads/02_fork_join_any.sv
+++ 05_processes or threads/02_fork_join_any.sv
@@ -34,9 +34,17 @@ module fork_join;
       
     join_any
     
-    //fork join_any  can complete and go to next block or process whwn any one the process complete inside the for join_any but still all the process will be executed in the fork join_any
+    /*fork join_any  can complete and go to next block or process
+    when any one of the process complete inside the for join_any but still 
+    all the process will be executed in the fork join_any*/
     
-      $display("fork join_any completed at time %t",$time);
+    //create a counter
+    $display();
+    for(int i =0;i<=7;i++)begin
+      $display(" i = %d",i);
+    end
+    
+    $display("\nfork join_any completed at time %t",$time);
     
   end
 endmodule
@@ -49,6 +57,16 @@ block 3 starts at time                    0
 block 4 starts at time                    0 
 
 block 1 executed at time                    5 
+ 
+ i =           0
+ i =           1
+ i =           2
+ i =           3
+ i =           4
+ i =           5
+ i =           6
+ i =           7
+
 fork join_any completed at time             5
 
 block 2 executed at time                   10 
