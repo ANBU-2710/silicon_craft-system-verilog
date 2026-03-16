cf286948c8a847dbf8f64ad43f93d1193199d478
ANBUSELVAM V <anbuselvamv27@gmail.com>
2026-03-16 12:00:30 +0530

    Update 01_static_array_rand.sv

--- 09_randomization/04_array_randomization/01_static_array_rand.sv
+++ 09_randomization/04_array_randomization/01_static_array_rand.sv
@@ -3,6 +3,10 @@
 class array;
   rand bit [3:0] arr [2:0][3:0];
   
+  constraint cons{
+    foreach (arr[i])
+      arr[i].sum() with (int'(item))==12;};
+  
 endclass
 
 module rand_array;
@@ -21,13 +25,3 @@ module rand_array;
   end
   
 endmodule
-
-
-
-/*
-the rand of array = '{'{'h9, 'hc, 'h7, 'h3}, '{'hc, 'h4, 'h9, 'h3}, '{'h4, 'h6, 'h2, 'h9}} 
-the rand of array = '{'{'hb, 'h0, 'ha, 'h2}, '{'h5, 'h1, 'ha, 'hd}, '{'hd, 'h4, 'h9, 'ha}} 
-the rand of array = '{'{'ha, 'h6, 'h3, 'he}, '{'ha, 'h0, 'h9, 'hb}, '{'h4, 'h0, 'h1, 'hc}} 
-the rand of array = '{'{'h7, 'h2, 'h9, 'h5}, '{'h0, 'h6, 'hc, 'h7}, '{'he, 'ha, 'h7, 'h1}} 
-the rand of array = '{'{'h0, 'hc, 'h4, 'hc}, '{'h9, 'hf, 'he, 'h2}, '{'h8, 'h4, 'hf, 'hb}} 
-*/
